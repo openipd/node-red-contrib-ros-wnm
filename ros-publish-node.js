1d9a69aa1c4c1153cd74a4f172a4cfaf952de1a1
Andreas Binder <abi@s-pe.eu.com>
2019-09-12 15:00:49 +0200

    update

--- ros-publish-node.js
+++ ros-publish-node.js
@@ -19,7 +19,7 @@ module.exports = function(RED) {
     node.on('input', (msg) => {
       topic.ros = node.server.ros;
       node.log('publishing msg ' + msg.payload);
-      var pubslishMsg = new ROSLIB.Message({data: msg.payload});
+      //var pubslishMsg = new ROSLIB.Message({data: msg.payload});
       topic.publish({data: msg.payload});
       //var pubslishMsg = new ROSLIB.Message(msg.payload);
       //topic.publish(pubslishMsg);    });
